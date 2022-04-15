40c2f5e4bd0754b01b5ae0ed946e968479eaa8d9
Roman Kostyuchenko <elromankos@gmail.com>
2022-04-15 23:56:53 +0200

    Principal roots create
    AppRouting Module update
    router outlet settled in app component
    Error page works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
