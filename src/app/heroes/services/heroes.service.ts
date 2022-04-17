d57888a101bc6dd44300b5b8709a3ec0a3a410aa
Roman Kostyuchenko <elromankos@gmail.com>
2022-04-17 22:51:58 +0200

    ListadoComponent service works in progress
    heroesService injection
    Heroe Interface create and modify, import
    Return Observable on Service
    View component construction
    Property heroes: Heroes[] establish

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { Heroe } from '../interface/heroes.interface';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,8 +12,8 @@ export class HeroesService {
 
   constructor( private http: HttpClient) { }
 
-  getHeroes() {
-   return this.http.get(`localhost:3000/heroes`)
+  getHeroes(): Observable<Heroe[]> {
+   return this.http.get<Heroe[]>(`http://localhost:3000/heroes`)
   }
 
 
