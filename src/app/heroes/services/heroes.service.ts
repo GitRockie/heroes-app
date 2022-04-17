3dd9c7297ed3a05e6bb8e2b6f609628366fc894d
Roman Kostyuchenko <elromankos@gmail.com>
2022-04-17 17:23:32 +0200

    Service works on process
    HttpClientModule install globaly
    HttpClient injection for testing
    HttpClient works

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class HeroesService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getHeroes() {
+   return this.http.get(`localhost:3000/heroes`)
+  }
+
+
+
+
+
+
+
+
 }
